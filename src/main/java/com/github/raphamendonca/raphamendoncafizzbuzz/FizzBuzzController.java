e1510c9ecfc66c8dc2a630b2bbd7a743e66561d5
Raphael Mendonça <raphamendonca@protonmail.com>
2019-09-28 14:02:53 -0300

    Optimize some Operations, removing target direcotry

--- src/main/java/com/github/raphamendonca/raphamendoncafizzbuzz/FizzBuzzController.java
+++ src/main/java/com/github/raphamendonca/raphamendoncafizzbuzz/FizzBuzzController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 import javax.validation.constraints.NotEmpty;
 
 import org.springframework.http.MediaType;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RequestMapping("fizzBuzz")
 @RestController
@@ -24,18 +28,20 @@ public class FizzBuzzController {
 		return validateFizzBuzz(IntStream.range(0, 100).boxed().collect(Collectors.toList()));
 	}
 
+	@ApiOperation(value = "Check if a given number is Fizz, Buzz or FizzBuzz")
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/{number}")
 	public List<FizzBuzz> checkFizzBuzz(
-			@NotEmpty(message = "The informed values must be a list of integers") @PathVariable("number") List<Integer> numbers) {
+			@ApiParam(value = "Number to be FizzBuzz check")
+			@NotEmpty(message = "The informed values must be a list of integers") 
+			@PathVariable("number") 
+			List<Integer> numbers) {
 		return validateFizzBuzz(numbers);
 	}
 
 	private List<FizzBuzz> validateFizzBuzz(List<Integer> numbers) {
 		List<FizzBuzz> fizzBuzzList = new ArrayList<>();
-
 		numbers.stream().forEach(number -> fizzBuzzList.add(new FizzBuzz(number)));
-
-		return fizzBuzzList;
+		return fizzBuzzList;	
 	}
 
 	@ExceptionHandler
